9d1e0556456a78bcf339e815fdc9a8c6f19c55b5
Tutku Ince <tutku.ince@outlook.com>
2023-12-17 22:20:35 +0300

    Type Assertions with the DOM

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
 "use strict";
 const btn = document.getElementById("btn");
+const input = document.getElementById("todoInput");
+// S18047: btn is possibly null, so we need to use null check
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
+    alert(input.value);
+    input.value = "";
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn")! as HTMLButtonElement;
+const input = document.getElementById("todoInput")! as HTMLInputElement;
+
+// S18047: btn is possibly null, so we need to use null check
+btn?.addEventListener("click", () => {
+    alert(input.value);
+    input.value = "";
+});
