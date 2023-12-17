9f322d84fbdb7615aab7d1549e5b73b94d26ce4f
Tutku Ince <tutku.ince@outlook.com>
2023-12-17 22:33:45 +0300

    building the todo list

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,21 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoInput");
 const form = document.querySelector('#idTodoForm');
+const list = document.getElementById("todoList");
 const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submitted!");
+    const newTodoText = input.value;
+    const newLI = document.createElement("LI");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLI.append(newTodoText);
+    newLI.append(checkbox);
+    list.append(newLI);
+    input.value = "";
 };
 form.addEventListener("submit", handleSubmit);
 // S18047: btn is possibly null, so we need to use null check
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
-    alert(input.value);
-    input.value = "";
-});
+// btn?.addEventListener("click", () => {
+//     alert(input.value);
+//     input.value = "";
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoInput")! as HTMLInputElement;
 const form = document.querySelector('#idTodoForm') as HTMLFormElement;
+const list = document.getElementById("todoList")!;
 
 const handleSubmit = (e: SubmitEvent) => {
     e.preventDefault();
-    console.log("Submitted!");
+    const newTodoText = input.value;
+    const newLI = document.createElement("LI");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLI.append(newTodoText);
+    newLI.append(checkbox);
+    list.append(newLI);
+    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
 // S18047: btn is possibly null, so we need to use null check
-btn?.addEventListener("click", () => {
-    alert(input.value);
-    input.value = "";
-});
+// btn?.addEventListener("click", () => {
+//     alert(input.value);
+//     input.value = "";
+// });
