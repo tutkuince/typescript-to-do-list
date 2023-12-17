590a24bf9a22c9679119bf215c43b4ab4c7276d0
Tutku Ince <tutku.ince@outlook.com>
2023-12-17 22:48:43 +0300

    connecting to LocalStorage

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,31 @@ const btn = document.getElementById("btn");
 const input = document.getElementById("todoInput");
 const form = document.querySelector('#idTodoForm');
 const list = document.getElementById("todoList");
-const handleSubmit = (e) => {
-    e.preventDefault();
-    const newTodoText = input.value;
+const readTodos = () => {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+};
+const createTodo = (todo) => {
     const newLI = document.createElement("LI");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
+};
+const todos = readTodos();
+todos.forEach(createTodo);
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
     input.value = "";
 };
 form.addEventListener("submit", handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,46 @@
+interface Todo {
+    text: string;
+    completed: boolean;
+}
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoInput")! as HTMLInputElement;
 const form = document.querySelector('#idTodoForm') as HTMLFormElement;
 const list = document.getElementById("todoList")!;
 
-const handleSubmit = (e: SubmitEvent) => {
-    e.preventDefault();
-    const newTodoText = input.value;
+const readTodos = (): Array<Todo> => {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null) return [];
+    return JSON.parse(todosJSON);
+}
+
+const createTodo = (todo: Todo) => {
     const newLI = document.createElement("LI");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
+}
+
+const todos: Array<Todo> = readTodos();
+todos.forEach(createTodo);
+
+const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+
     input.value = "";
 }
+
+
+
 form.addEventListener("submit", handleSubmit);
 // S18047: btn is possibly null, so we need to use null check
 // btn?.addEventListener("click", () => {
