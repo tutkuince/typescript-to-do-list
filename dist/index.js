6e85827c47c8b535d6830a812d8e698beb9db161
Tutku Ince <tutku.ince@outlook.com>
2023-12-17 22:26:19 +0300

    Working with Events

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,12 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoInput");
+const form = document.querySelector('#idTodoForm');
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted!");
+};
+form.addEventListener("submit", handleSubmit);
 // S18047: btn is possibly null, so we need to use null check
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
     alert(input.value);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoInput")! as HTMLInputElement;
+const form = document.querySelector('#idTodoForm') as HTMLFormElement;
 
+const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    console.log("Submitted!");
+}
+form.addEventListener("submit", handleSubmit);
 // S18047: btn is possibly null, so we need to use null check
 btn?.addEventListener("click", () => {
     alert(input.value);
